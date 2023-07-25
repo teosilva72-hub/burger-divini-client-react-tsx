97b02350b61f09030c79d7befc7d319923a3cf93
teosilva72 <representate.com.sp@gmail.com>
2023-07-24 22:38:01 -0300

    modified:   src/app/view/login.tsx 	new file:   src/app/view/modal/registerUser.tsx

--- src/app/view/login.tsx
+++ src/app/view/login.tsx
@@ -4,9 +4,13 @@ import '../assets/css/login.css';
 import auth from "../api/auth";
 import { toast } from 'react-toastify';
 import { useNavigate, useNavigation } from 'react-router-dom';
+import ModalRegisterUser from './modal/registerUser';
+import $ from 'jquery';
 
 export default function Login() {
-
+    $('.background').click(()=>{
+        alert()
+    })
     const navigate = useNavigate();
 
     const [email, setEmail] = useState<string>("");
@@ -14,8 +18,8 @@ export default function Login() {
 
     const submit = async (e: any) => {
         e.preventDefault();
-        
-        const result:any = await auth.login(email, password);
+
+        const result: any = await auth.login(email, password);
         if (result.status) {
             document.cookie = `Bearer ${result.token}`;
             toast.success(result.message, {
@@ -39,6 +43,11 @@ export default function Login() {
 
     return (
         <>
+        <ModalRegisterUser
+                title="Registrar-se!"
+                closed="Cancelar"
+                submit="Cadastrar"
+            />
             <div className='background'></div>
             <div className="main">
                 <form onSubmit={submit}>
@@ -68,12 +77,24 @@ export default function Login() {
                                     required
                                 />
                             </div>
-                            <button type="submit" className="btn btn-primary col-12">Acessar</button>
+                            <button type="submit" className="btn btn-primary col-12 mb-3">Acessar</button>
+                            <div className="row">
+                                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modalRegisterUser">
+                                    Launch demo modal</button>
+                                <div className="col-12">
+                                    <a href="#" data-toggle="modal" data-target="#modalRegisterUser"
+                                        className="list-group-item list-group-item-action text-center">Recuperar senha</a>
+                                </div>
+                                <div className="col-12">
+                                    <a href="#" className="list-group-item list-group-item-action text-center">Primeiro Acesso</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
                 </form>
             </div>
+            
         </>
     );
 }
