9588e8c2d5d539b3ea2e3ec870220755788c9e05
teosilva72 <representate.com.sp@gmail.com>
2024-06-24 18:14:41 -0300

    modified:   .gitignore 	new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/robots.txt 	new file:   build/static/css/main.703ad315.css 	new file:   build/static/css/main.703ad315.css.map 	new file:   build/static/js/787.c4e7f8f9.chunk.js 	new file:   build/static/js/787.c4e7f8f9.chunk.js.map 	new file:   build/static/js/main.5e8dcce5.js 	new file:   build/static/js/main.5e8dcce5.js.LICENSE.txt 	new file:   build/static/js/main.5e8dcce5.js.map 	new file:   build/static/media/bootstrap-icons.4753c5ba57962b4d7bf8.woff 	new file:   build/static/media/bootstrap-icons.6d63d0501e5ed7b79dab.woff2 	modified:   src/app/view/index.tsx

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import '../assets/css/index.css';
-import { Chart } from "react-google-charts";
-import apiUser from "../api/user";
-import apiProduto from '../api/produto';
+
 import { toast } from "react-toastify";
 import $ from 'jquery';
 import Menu01 from "./component/menu";
 
 export default function Index() {
 
-    
+    const anotaAI = (link:any) => {
+       return window.location.href = link
+    }
 
     return (
         <>
            
             <Menu01/><br /><br /><br /><br />
-            <iframe src="https://app.anota.ai/m/HTN5SmxJw" width="100%" height="650px" style={{border: "none"}}></iframe>
+            <iframe  src="https://app.anota.ai/m/HTN5SmxJw" width="100%" height="650px" style={{border: "none"}}></iframe>
 
         </>
     );
