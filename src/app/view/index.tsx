1be2626e100ee9e7cac43530a23ede13e2451096
teosilva72 <representate.com.sp@gmail.com>
2024-06-24 17:37:37 -0300

    modified:   src/app/view/index.tsx

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -14,8 +14,8 @@ export default function Index() {
     return (
         <>
            
-            <Menu01/><br /><br /><br />
-            <iframe src="https://app.anota.ai/m/HTN5SmxJw" width="100%" height="500px" style={{border: "none"}}></iframe>
+            <Menu01/><br /><br /><br /><br />
+            <iframe src="https://app.anota.ai/m/HTN5SmxJw" width="100%" height="650px" style={{border: "none"}}></iframe>
 
         </>
     );
