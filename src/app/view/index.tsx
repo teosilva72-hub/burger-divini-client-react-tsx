d14032daec4b26946b3f8895c8a7bb593a7eea9b
teosilva72 <representate.com.sp@gmail.com>
2024-06-30 20:43:44 -0300

    new file:   src/app/assets/img/logotipo.jpg 	modified:   src/app/view/index.tsx

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../assets/css/index.css';
 
 import { toast } from "react-toastify";
@@ -8,25 +8,71 @@ import Carrosel01 from "../view/component/carrossel"
 import ModalBemVinda from "./component/mod/modalBemVinda";
 import locale from "../assets/locale/Pt_BR.json"
 import card01 from "./component/card/card01";
-
+declare global {
+    interface Window {
+      gtag?: (...args: any[]) => void;
+    }
+  }
 
 export default function Index() {
+
+    const useGTM = (trackingId: string) => {
+        useEffect(() => {
+            try {
+                const script = document.createElement('script');
+                script.async = true;
+                script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
+                document.head.appendChild(script);
+
+                const inlineScript = document.createElement('script');
+                inlineScript.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${trackingId}');
+          `;
+                document.head.appendChild(inlineScript);
+
+                return () => {
+                    document.head.removeChild(script);
+                    document.head.removeChild(inlineScript);
+                };
+            } catch (e: any) {
+                console.log("error google::: ", e.toString())
+            }
+        }, [trackingId]);
+    };
+    const gtagReportConversion = (): boolean => {
+       
+          if (window.gtag) {
+            window.gtag('event', 'conversion', {
+              send_to: 'AW-16531069974/Gq2mCNbq37oZEJa40Mo9',
+              transaction_id: ''
+            });
+          }
+
+        return false;
+    };
+
+    useGTM("AW-16531069974")
+    
 
     const welcome = () => {
         $('#modalWelcome').click()
     }
-    
-    setTimeout(()=>{
+
+    setTimeout(() => {
         welcome()
-    },100)
+        gtagReportConversion()
+    }, 100)
 
     return (
         <>
-            <Menu01 />
-            <ModalBemVinda title={locale.empresa} close={locale.modal.close} component={card01}/>
+            <Menu01/>
+            <ModalBemVinda title={locale.empresa} close={locale.modal.close} component={card01} />
             <div className="content">
-                <Carrosel01 />
-                <iframe src={locale.integrator.anotaAi} width="100%" height="650px" style={{border: "none"}}></iframe>
+                <Carrosel01/>
+                <iframe src={locale.integrator.anotaAi} width="100%" height="650px" style={{ border: "none" }}></iframe>
                 <button type="button" id="modalWelcome" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     Launch static backdrop modal
                 </button>
